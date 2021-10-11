5e2ab2b4a17c430f6e1f17e58c826994f35b36e1
Daniel McGrath <danmcgrath1035@gmail.com>
2021-10-11 16:45:28 -0400

    adding simple filter mask

---
+++ partB.m
@@ -0,0 +1,43 @@
+% directory = uigetdir(pwd, 'Select a Folder');
+% filePattern = fullfile(directory, '*.jpg');
+filePattern = '/Users/danielmcgrath/Documents/fifth-year-northeastern/Fall/computer-vision/Project 1/cvp1/Office/*.jpg';
+files = dir(filePattern);
+numFiles = length(files);
+threshold = 3;
+tsigma = 1;
+
+images = zeros(240,320,1070);
+for i = 1:numFiles
+   file = files(i);
+   images(:,:,i) = rgb2gray(imread(strcat(file.folder,'/',file.name)));
+end
+
+file = images(:,:,200);
+figure;
+colormap gray;
+image(file, 'CDataMapping', 'scaled');
+
+temporal_d = zeros(240, 320, 1069);
+simple_filter_masked = simple_filter(images, numFiles, threshold);
+
+
+function [simple_filter_masked] = simple_filter(images, numFiles, threshold)
+    for i = 2:(numFiles - 2)
+        img_0 = images(:,:,i-1);
+        img_1 = images(:,:,i);
+        img_2 = images(:,:,i + 1);
+        diff = 0.5 * ((img_0 .* -1) + (img_1 .* 0) + (img_2 .* 1));
+        zeroindices = find(abs(diff) < threshold);
+        oneindices = find(abs(diff) >= threshold);
+        maskedDiff = diff; 
+        maskedDiff(zeroindices) = 0;
+        maskedDiff(oneindices) = 1;
+        simple_filter_masked(:,:,i) = maskedDiff;
+    end
+end
+
+
+
+
+
+    
