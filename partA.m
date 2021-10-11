d77d298182cf4dd4eee5aeeac4212be561760e3b
Daniel McGrath <danmcgrath1035@gmail.com>
2021-10-11 14:42:13 -0400

    changes

--- partA.m
+++ partA.m
@@ -4,9 +4,14 @@ filePattern = '/Users/danielmcgrath/Documents/fifth-year-northeastern/Fall/compu
 officeFiles = dir(filePattern);
 numOfficeFiles = length(officeFiles);
 
-images = zeros(240,320,3,1070);
+images = zeros(240,320,1070);
 for i = 1:numOfficeFiles
    file = officeFiles(i);
-   images(:,:,:,i) = imread(strcat(file.folder,'/',file.name));
+   images(:,:,i) = rgb2gray(imread(strcat(file.folder,'/',file.name)));
 end
+
+   file = images(:,:,200);
+   figure;
+   colormap gray;
+   image(file, 'CDataMapping', 'scaled');
 
